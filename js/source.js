fe65fd6eb35e1433007cf8d78a53bc3c2ebbe4ab
jreyes-13 <jonatan.reyes01@utrgv.edu>
2026-09-14 12:22:16 -0500

    Added instructions

--- js/source.js
+++ js/source.js
@@ -1,5 +1,4 @@
- 
-  var hotel = {
+var hotel = {
     name: 'Park',
     roomRate: 240, // Amount in dollars
     discount: 15,  // Percentage discount
@@ -7,11 +6,28 @@
       var offerRate = this.roomRate * ((100 - this.discount) / 100);
       return offerRate;
     }
-  };
+};
 
- 
-  var hotelName, roomRate, specialRate;                    
 
-  
+var hotelName, roomRate, specialRate;                   
+
+// TODO: Use either the traditional or modern DOM selector methods to retrieve
+// the HTML elements with the following IDs: hotelName, roomRate, and specialRate.
+// Assign each element to its corresponding JavaScript variable.
+
+
+
+// TODO: Use the three JavaScript variables to update the text content of
+// their corresponding HTML elements using information from the hotel object.
+// NOTE: Two variables will be assigned text values directly, while the
+// third variable will use a JavaScript function to generate its value.
+
+
+
+// TODO: Use either the traditional or modern DOM selector methods to retrieve
+// the HTML element with the ID: offerEnds.
+// Then, replace its text with the following hard-coded message:
+// "Offer expires next Monday (September 21, 2026)"
+
 
 
